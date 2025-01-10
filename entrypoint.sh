098c2cd9f7b51ffe9a48068b6dd6472676d1d70f
Kevin Funke <funke@validierung.cc>
2025-01-10 17:15:54 +0100

    fixed node stopping because of missing parameter no-console

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -meuo pipefail
 
 if [ $# -eq 0 ]; then
-  /zano/zano/build/src/zanod --rpc-bind-ip=0.0.0.0 --rpc-bind-port=11211
+  /zano/zano/build/src/zanod --rpc-bind-ip=0.0.0.0 --rpc-bind-port=11211 --no-console
 else
   exec "$@"
 fi
